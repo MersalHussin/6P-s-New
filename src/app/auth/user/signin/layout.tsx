a37d8b719647f58ca012c4d8112bada8754fdfcf
Mersal Hussin <mersalhussin2004@gmail.com>
2025-09-21 01:17:01 +0000

    بتظهر المشكلة دي :
    starting build "da6bbf42-1c98-4271-8fae-e8d475ad0cd2

---
+++ src/app/auth/user/signin/layout.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Suspense } from 'react';
+
+export default function SignInLayout({ children }: { children: React.ReactNode }) {
+  return <Suspense>{children}</Suspense>;
+}
